0e0405570ea3571f3c0bf4462da6241472e3efd8
Moses <moses.maneesh@gmail.com>
2020-05-06 23:19:53 +0530

    Completed Dashboard with responsive sizes

--- app.js
+++ app.js
@@ -1,13 +1,161 @@
-var bar_status=true;
+var bar_status = true;
 
-function toggleSidebar(){
-    if(bar_status){
-        $("#mySidebar").css('width','16vmax')
-        $("#main").css('marginLeft','16vmax')
-        this.bar_status=false;
-    }else{
-        $("#mySidebar").css('width','4.5vmax')
-        $("#main").css('marginLeft','4.5vmax')
-        this.bar_status=true;
+function toggleSidebar() {
+    if (bar_status) {
+        $("#mySidebar").css('width', '16vmax')
+        $("#main").css('marginLeft', '16vmax')
+        this.bar_status = false;
+    } else {
+        $("#mySidebar").css('width', '4.5vmax')
+        $("#main").css('marginLeft', '2vmax')
+        this.bar_status = true;
     }
 }
+
+$(document).ready(function() {
+    function randomScalingFactor() {
+        return (Math.random());
+    }
+
+    function onRefresh(chart) {
+        chart.config.data.datasets[0].data.push({
+            x: Date.now(),
+            y: randomScalingFactor()
+        });
+    }
+
+    function onRefresh2(chart) {
+        chart.config.data.datasets[0].data.push({
+            x: Date.now(),
+            y: randomScalingFactor()
+        });
+    }
+
+    var ctx = document.getElementById("sens-1").getContext('2d')
+    var ctx2 = document.getElementById("sens-2").getContext('2d')
+
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            datasets: [{
+                backgroundColor: '#ec250d',
+                borderColor: '#ec250d',
+                lineTension: 0,
+                fill: false,
+                // borderDash:[8,4],
+                data: []
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+            // title:{
+            //     display:true,
+            //     text:'Charrt'
+            // },
+
+            scales: {
+                xAxes: [{
+                    type: 'realtime',
+                    realtime: {
+                        duration: 20000,
+                        refresh: 1000,
+                        delay: 2000,
+                        onRefresh: onRefresh
+                    },
+                    gridLines: {
+                        drawOnChartArea: true
+                    }
+                }],
+
+                yAxes: [{
+                    scaleLabel: {
+                        display: false,
+                        labelString: 'value'
+                    },
+                    gridLines: {
+                        drawOnChartArea: true
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            },
+            tooltips: {
+                mode: 'nearest',
+                intersect: false
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: false
+            },
+            maintainAspectRatio: false
+        }
+    });
+
+    var chart2 = new Chart(ctx2, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            datasets: [{
+                backgroundColor: '#ec250d',
+                borderColor: '#ec250d',
+                lineTension: 0,
+                fill: false,
+                // borderDash:[8,4],
+                data: []
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+            // title:{
+            //     display:true,
+            //     text:'Charrt'
+            // },
+
+            scales: {
+                xAxes: [{
+                    type: 'realtime',
+                    realtime: {
+                        duration: 20000,
+                        refresh: 1000,
+                        delay: 2000,
+                        onRefresh: onRefresh
+                    },
+                    gridLines: {
+                        drawOnChartArea: true
+                    }
+                }],
+
+                yAxes: [{
+                    scaleLabel: {
+                        display: false,
+                        labelString: 'value'
+                    },
+                    gridLines: {
+                        drawOnChartArea: true
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            },
+            tooltips: {
+                mode: 'nearest',
+                intersect: false
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: false
+            },
+            maintainAspectRatio: false
+        }
+    });
+
+})
